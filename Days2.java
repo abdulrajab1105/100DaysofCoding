112b92973159f5377e203e805792ce649475f411
abdulrajab1105 <kinggalaxy1105@gmail.com>
2025-09-09 15:11:35 +0800

    penggunaab println, print dan printf

---
+++ Days2.java
@@ -0,0 +1,7 @@
+public class days2 {
+    public static void main(String[] args) {
+        System.out.println("println");
+        System.out.print("print");
+        System.out.printf("%nabdul %s!","Rajab");
+    }
+}
