a7975805ba8efcacb74cd99f7b68d0503bd004a3
abdulrajab1105 <kinggalaxy1105@gmail.com>
2025-09-11 14:40:09 +0800

    komentar

---
+++ days3.java
@@ -0,0 +1,7 @@
+public class days3 {
+    public static void main(String[] args) {
+        System.out.println("println"); // penggunaan println
+        System.out.print("print"); /*penggunaan print*/
+        System.out.printf("%nabdul %s!","Rajab");
+    }
+}
